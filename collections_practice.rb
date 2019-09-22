e316194a4cbb4ed295a554a99e67f8e8acf2f7aa
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 14:31:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc
+   puts(sort_array_asc([25, 7, 1])=([1,7,25])
+  end
+
+  def sort_array_desc
+      puts (sort_array_desc([25, 7, 14]))=([25, 14, 7])
+  end
+
+  def sort_array_char_count
+    puts (sort_array_char_count(["dogs", "cat", "Horses"]))=(["cat", "dogs", "Horses"])
+  end
+
+  def swap_elements
+    swap_elements(["blake", "ashley", "scott"])=(["blake", "scott", "ashley"])
+  end
+
+  def reverse_array
+      reverse_array([12, 4, 35])=([35, 4, 12])
+    end
+  
+  def kesha_maker
+      kesha_maker(["blake", "ashley", "scott"])=(["bl$ke", "as$ley", "sc$tt"])
+    end
+
+
+  def find_a
+      find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ])=(["apple", "avis", "arlo", "ascot"])
+    end
+
+
+  def sum_array
+   sum_array([11,4,7,8,9,100,134])=(273)
+    end
+
+
+  def add_s
+      add_s(["hand","feet", "knee", "table"])=(["hands","feet", "knees", "tables"])
+end
 
