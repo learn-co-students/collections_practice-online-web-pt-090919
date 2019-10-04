cd6d5556b73f5791603bea5bbd119039d0cede27
Ally Morson <ally.morson@gmail.com>
2019-10-04 04:27:04 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,10 @@
+numbers = [2,5,1,6,3,9,7,8]
+def sort_array_asc(numbers)
+numbers.sort
+end
+sort_array_asc(numbers)
+end
 
+def sort_array_desc(numbers)
+  numbers.sort do |num| num.reverse
+endnumbers = [25,7,14]
